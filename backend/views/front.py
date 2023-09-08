4a1f7aa271cd66a8061cebeaf02bc245f467a139
RedHot099 <redhotdevil996@gmail.com>
2023-09-08 14:11:19 +0200

    add google oauth2 login & user roles dependent display

--- backend/views/front.py
+++ backend/views/front.py
@@ -2,19 +2,28 @@ from django.shortcuts import render
 from ..models import Zaplecze
 from ..serializers import ZapleczeSerializer
 from django.views.generic import View
-
+from allauth.socialaccount.models import SocialAccount
 
 
 class Front(View):
     def get(self, request):
         queryset = Zaplecze.objects.values()
-        context = {'queryset': queryset}
+        try:
+            data = SocialAccount.objects.get(user=request.user).extra_data
+        except:
+            data = {}
+        context = {'queryset': queryset, 'social_data': data}
         return render(request, 'index.html', context)
     
 
 class CreateZaplecze(View):
     def get(self, request):
-        return render(request, 'create.html')
+        try:
+            data = SocialAccount.objects.get(user=request.user).extra_data
+        except:
+            data = {}
+        context = {'social_data': data}
+        return render(request, 'create.html', context)
     
 
 class ZapleczeUnit(View):
