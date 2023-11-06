db0c61e41afd305961b3c9231b256682e5d3b0af
Dragone <arcxar@gmail.com>
2023-11-06 12:27:09 +0100

    color picker
    added rgb picker (front and back)

--- backend/static/js/zaplecze_classic.js
+++ backend/static/js/zaplecze_classic.js
@@ -1,9 +1,22 @@
+function updateRGB(value) {
+             
+    var r = parseInt(value.substring(1, 3), 16);
+    var g = parseInt(value.substring(3, 5), 16);
+    var b = parseInt(value.substring(5, 7), 16);
+    var rgbValue = r + "," + g + "," + b;
+    $('#rgbValue').val(rgbValue);
+
+    console.log(rgbValue);
+}
+
 $(document).ready(function() {
     var cardId = $('#classic_main').data('card-id');
 
     $('button[type="submit_tsv"]').on('click', function(event) {
         event.preventDefault();
         console.log('Zaliczyło!');
+        
+        var colorValue = $('#rgbValue').val();
 
         const data = {
             tsvInput: $('#tsvInput').val(),
@@ -12,8 +25,10 @@ $(document).ready(function() {
             dateInput: $('#dateInput').val(),
             publishInterval: $('#publishInterval').val(),
             openai_api_key: $('#openai_api_key_classic').val(),
-            faqOption: $('input[name="faqOption"]:checked').val()
+            faqOption: $('input[name="faqOption"]:checked').val(),
+            overlayColor: colorValue
         };
+
 
         $.ajax({
             url: '/api/create/' + cardId + '/zaplecze_classic/',
@@ -33,37 +48,39 @@ $(document).ready(function() {
         });
     });
 
-    // function getWordpressPostsTitles() {
+    function getWordpressPostsTitles() {
 
-    //     const password = document.getElementById('wp_api_key').innerText;
-    //     const domain = document.getElementById('domain').innerText;
-    //     const login = document.getElementById('wp_user').innerText;
+        const password = document.getElementById('wp_api_key').innerText;
+        const domain = document.getElementById('domain').innerText;
+        const login = document.getElementById('wp_user').innerText;
       
-    //     const token = btoa(`${login}:${password}`);
-    //     const authHeader = { 'Authorization': `Basic ${token}` };
+        const token = btoa(`${login}:${password}`);
+        const authHeader = { 'Authorization': `Basic ${token}` };
       
-    //     const apiUrl = `https://${domain}/wp-json/wp/v2/posts`;
-    //     console.log(apiUrl);
+        const apiUrl = `https://${domain}/wp-json/wp/v2/posts`;
+        console.log(apiUrl);
       
-    //     fetch(apiUrl, { headers: authHeader })
-    //       .then(response => {
-    //         if (!response.ok) {
-    //           throw new Error(`HTTP error! status: ${response.status}`);
-    //         }
-    //         return response.json();
-    //       })
-    //       .then(posts => {
-    //         console.log('Tytuły wpisów:');
-    //         posts.forEach(post => {
-    //           console.log(post.title.rendered);
-    //         });
-    //       })
-    //       .catch(error => {
-    //         console.error('Wystąpił błąd podczas pobierania wpisów:', error);
-    //       });
-    //   }
+        fetch(apiUrl, { headers: authHeader })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+          })
+          .then(posts => {
+            console.log('Tytuły wpisów:');
+            posts.forEach(post => {
+              console.log(post.title.rendered);
+            });
+          })
+          .catch(error => {
+            console.error('Wystąpił błąd podczas pobierania wpisów:', error);
+          });
+      }
       
-    //   setInterval(getWordpressPostsTitles, 30000);
+      setInterval(getWordpressPostsTitles, 600000);
+
+    
 
     function getCookie(name) {
         let cookieValue = null;
